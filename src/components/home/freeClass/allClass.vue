68fe2ac9e96f22ae47e6dc644d33c6ffd1150f4f
lyq7743 <lyq7743@163.com>
2019-08-01 10:41:54 +0800

    修改免费课程的bug

--- src/components/home/freeClass/allClass.vue
+++ src/components/home/freeClass/allClass.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <other-class v-for="(item,index) in shu0"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
+    <other-class v-for="(item,index) in shu0":key = "item.list"   :people='item.people' :classprice="item.price" :classSrc='item.img_src'
       :className="item.class_title" :classlevel='item.class_level'>
     </other-class>
 
-   <other-class v-for="(item,index) in shu1"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
+   <other-class v-for="(item,index) in shu1" :key = "item.list"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
       :className="item.class_title" :classlevel='item.class_level'>
     </other-class>
-    <other-class v-for="(item,index) in shu2"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
+    <other-class v-for="(item,index) in shu2":key = "item.list"   :people='item.people' :classprice="item.price" :classSrc='item.img_src'
       :className="item.class_title" :classlevel='item.class_level'>
     </other-class>
-    <other-class v-for="(item,index) in shu3"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
+   <other-class v-for="(item,index) in shu3" :key = "item.list"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
       :className="item.class_title" :classlevel='item.class_level'>
     </other-class>
 
@@ -35,7 +35,7 @@
     mounted() {
       this.$axios.get('https://www.easy-mock.com/mock/5d40e999e63c672d5de1a35a/json/freeClass')
         .then(res => {
-          // console.log(res.data.class_list);
+          console.log(res.data.class_list);
 
           this.shuju = res.data.class_list;
           this.shu0 = res.data.class_list[0].list;
