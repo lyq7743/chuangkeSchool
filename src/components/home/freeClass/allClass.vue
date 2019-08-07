2147f71c318b189c97312a2cf3c84944b4a76043
lyq7743 <lyq7743@163.com>
2019-08-07 12:31:30 +0800

    完善allClass、

--- src/components/home/freeClass/allClass.vue
+++ src/components/home/freeClass/allClass.vue
@@ -1,26 +1,12 @@
 <template>
   <div>
     <div  v-for="(item,index) in shuju" :key = "index" :id="index">
-       <other-class v-for="(item,index) in item" :key = "item.list" :id="index"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
-        :className="item.class_title" :classlevel='item.class_level' @click.native = 'fn(index)'>
+		
+       <other-class v-for="(citem,cindex) in item.list" :key = "cindex" :id="cindex"  :people='citem.people' :classprice="citem.price" :classSrc='citem.img_src'
+        :className="citem.class_title" :classlevel='citem.class_level' @click.native = 'fn(cindex)'>
       </other-class>
 
     </div>
-
-
-   <!-- <other-class v-for="(item,index) in shu0" :key = "item.list" :id="index"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
-      :className="item.class_title" :classlevel='item.class_level' @click.native = 'fn(index)'>
-    </other-class>
-
-   <other-class v-for="(item,index) in shu1" :key = "item.list"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
-      :className="item.class_title" :classlevel='item.class_level' @click.native = 'fn(index)'>
-    </other-class>
-    <other-class v-for="(item,index) in shu2" :key = "item.list"   :people='item.people' :classprice="item.price" :classSrc='item.img_src'
-      :className="item.class_title" :classlevel='item.class_level' @click.native = 'fn(index)'>
-    </other-class>
-   <other-class v-for="(item,index) in shu3" :key = "item.list"  :people='item.people' :classprice="item.price" :classSrc='item.img_src'
-      :className="item.class_title" :classlevel='item.class_level' @click.native = 'fn(index)'>
-    </other-class> -->
 
   </div>
 </template>
@@ -31,10 +17,7 @@
     data() {
       return {
         shuju: [],
-        // shu0: [],
-        // shu1: [],
-        // shu2: [],
-        // shu3: [],
+
       }
     },
     components: {
@@ -46,14 +29,7 @@
         .then(res => {
           console.log(res.data.class_list);
           console.log(res.data.class_list.length)
-
           this.shuju = res.data.class_list;
-
-
-          this.shu0 = res.data.class_list[0].list;
-          this.shu1 = res.data.class_list[1].list;
-          this.shu2 = res.data.class_list[2].list;
-          this.shu3 = res.data.class_list[3].list;
         })
     },
     methods:{
